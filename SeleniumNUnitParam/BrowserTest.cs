b2cddd303d0b3b5c39e414e8a435eb65cca861ba
developerprofiles <43913990+developerprofiles@users.noreply.github.com>
2020-07-28 18:11:41 +0530

    Update BrowserTest.cs

--- SeleniumNUnitParam/BrowserTest.cs
+++ SeleniumNUnitParam/BrowserTest.cs
@@ -9,8 +9,28 @@ using System.Threading.Tasks;
 namespace SeleniumNUnitParam
 {
     public class BrowserTest : Hooks
-    {
-
+    {  
+        [Test]
+        public void PluralsightTest()
+        {
+            Driver.Navigate().GoToUrl("http://www.google.com");
+            var searchText = Driver.FindElement(By.Id("lst-ib"));
+            searchText.SendKeys("Pluralsight");
+            searchText.SendKeys(Keys.Enter);
+            System.Threading.Thread.Sleep(2000);
+            Assert.That(Driver.PageSource.Contains("Pluralsight"), Is.EqualTo(true), "The text Pluralsight doest not exist");
+        }
+        
+        [Test]
+        public void ExecuteAutomationTest()
+        {
+            Driver.Navigate().GoToUrl("http://executeautomation.com/demosite/Login.html");
+            Driver.FindElement(By.Name("UserName")).SendKeys("admin");
+            Driver.FindElement(By.Name("Password")).SendKeys("admin");
+            Driver.FindElement(By.Name("Login")).Submit();
+            System.Threading.Thread.Sleep(2000);
+            Assert.That(Driver.PageSource.Contains("Selenium"), Is.EqualTo(true), "The text selenium doest not exist");
+        }
 
         [Test]
         public void GoogleTest()
@@ -19,24 +39,7 @@ namespace SeleniumNUnitParam
             Driver.FindElement(By.Name("q")).SendKeys("Selenium");
             System.Threading.Thread.Sleep(5000);
             Driver.FindElement(By.Name("btnG")).Click();
-            Assert.That(Driver.PageSource.Contains("Selenium"), Is.EqualTo(true),
-                                            "The text selenium doest not exist");
-
+            Assert.That(Driver.PageSource.Contains("Selenium"), Is.EqualTo(true), "The text selenium doest not exist");
         }
-
-        [Test]
-        public void ExecuteAutomationTest()
-        {
-            /*Driver.Navigate().GoToUrl("http://executeautomation.com/demosite/Login.html");
-            Driver.FindElement(By.Name("UserName")).SendKeys("admin");
-            Driver.FindElement(By.Name("Password")).SendKeys("admin");
-            Driver.FindElement(By.Name("Login")).Submit();
-            System.Threading.Thread.Sleep(2000);
-            Assert.That(Driver.PageSource.Contains("Selenium"), Is.EqualTo(true),
-                                            "The text selenium doest not exist");*/
-
-        }
-
-
     }
 }
